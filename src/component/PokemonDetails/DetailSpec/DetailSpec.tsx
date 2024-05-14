d1c6f77535eba09c17ddc8fae8ae5c7e4a4e2bcf
aru <shinichi.130707@gmail.com>
2024-05-14 14:07:17 +0900

    detailSpecのmap修正

--- src/component/PokemonDetails/DetailSpec/DetailSpec.tsx
+++ src/component/PokemonDetails/DetailSpec/DetailSpec.tsx
@@ -5,7 +5,6 @@ type prop = {
 }
 
 const DetailSpec = (props :prop) => {
-    // ここにループ
     return (
         <div className="detail__spec">
             <p className="detail__spec-name detail__font">{props.statsName}</p>
@@ -30,8 +29,8 @@ const DetailSpec = (props :prop) => {
                 <ul className="detail__spec-list">
                     {/* ここループで行けたらいい */}
                     {/* ここ確認したい */}
-                    {[...Array(Math.floor(props.stats / 15))].map((undefined,i) => {
-                        return  <li key = {i} className="detail__item-list is-value"></li>
+                    {[...Array(Math.floor(props.stats / 15))].map((_,i) => {
+                        return  <li key={i}  className="detail__item-list is-value"></li>
                     })}
                 </ul>
             </div>
